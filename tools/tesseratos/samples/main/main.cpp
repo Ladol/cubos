0fff7f29d9c2cfcbd3d18f8c95b39cf66d22b75a
tomas7770 <77364520+tomas7770@users.noreply.github.com>
2023-11-24 22:52:09 +0000

    feat(tesseratos): add setting for theme

--- tools/tesseratos/samples/main/main.cpp
+++ tools/tesseratos/samples/main/main.cpp
@@ -1,14 +1,11 @@
 #include <cubos/engine/settings/settings.hpp>
 
 #include <tesseratos/plugin.hpp>
-#include <tesseratos/themes/plugin.hpp>
 
 using cubos::core::ecs::Write;
 
 using cubos::engine::Cubos;
 using cubos::engine::Settings;
-
-using tesseratos::Themes;
 
 static void settingsSystem(Write<Settings> settings)
 {
@@ -16,16 +13,10 @@ static void settingsSystem(Write<Settings> settings)
     settings->setBool("assets.io.readOnly", false);
 }
 
-static void testTheme(Write<Themes> themes)
-{
-    themes->changeTheme(0);
-}
-
 int main(int argc, char** argv)
 {
     Cubos cubos{argc, argv};
     cubos.addPlugin(tesseratos::plugin);
     cubos.startupSystem(settingsSystem).tagged("cubos.settings");
-    cubos.startupSystem(testTheme).after("cubos.imgui.init");
     cubos.run();
 }
--- tools/tesseratos/src/tesseratos/themes/plugin.cpp
+++ tools/tesseratos/src/tesseratos/themes/plugin.cpp
@@ -1,13 +1,24 @@
 #include <cubos/engine/imgui/plugin.hpp>
+#include <cubos/engine/settings/settings.hpp>
 
 #include <tesseratos/themes/plugin.hpp>
 
+using cubos::core::ecs::Write;
+
 using cubos::engine::Cubos;
+using cubos::engine::Settings;
 
 using namespace tesseratos;
+
+static void initTheme(Write<Settings> settings, Write<Themes> themes)
+{
+    themes->changeTheme(settings->getInteger("tesseratos.theme", 0));
+}
 
 void tesseratos::themesPlugin(Cubos& cubos)
 {
     cubos.addPlugin(cubos::engine::imguiPlugin);
     cubos.addResource<Themes>();
+
+    cubos.startupSystem(initTheme).after("cubos.imgui.init");
 }
